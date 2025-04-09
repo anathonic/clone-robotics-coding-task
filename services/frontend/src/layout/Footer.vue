59928a379a822c3e28a7dffba817bec9307701ec
Anath <anathonic@gmail.com>
2025-04-09 10:14:08 +0200

    update Footer and Navigation

--- services/frontend/src/layout/Footer.vue
+++ services/frontend/src/layout/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full bg-gray-100 border-t-1 px-8 py-6 flex justify-end">
+  <div class="w-full bg-gray-100 border-t-1 border-gray-200 px-8 py-6 flex justify-end">
     <div class="flex items-center">
       <span class="mr-2 ml-2 text-xs font-semibold"> Clone Robotics © {{ date }}</span>
-      <img class="h-5 max-w-xs" src="../assets/png/clone-robotics-logo.png" alt="">
+      <img class="h-5 max-w-xs" src="@/assets/png/clone-robotics-logo.png" alt="">
     </div>
   </div>
 </template>
--- services/frontend/src/layout/Navigation.vue
+++ services/frontend/src/layout/Navigation.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="sticky top-0 z-40">
-    <div class="w-full h-28 px-6 bg-gray-100 border-b flex items-center justify-end">
+    <div class="w-full h-28 px-6 bg-gray-100 border-b border-gray-200 flex items-center justify-center">
       <div class="flex">
-        <img class="max-w-xs h-10" src="../assets/png/clone-robotics-logo.png" alt="">
+        <img class="max-w-xs h-10" src="@/assets/png/clone-robotics-logo.png" alt="">
       </div>
     </div>
   </div>
